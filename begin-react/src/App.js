47b73694e79e0532f33efa560d3414e99b2364ab
lazy-sky <1cielo@naver.com>
2021-11-22 00:05:20 +0900

    Props

---
+++ begin-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <>
+    <Hello name="react" color="red"/>
+    <Hello color="pink"/>
+    </>
+  );
+}
+
+export default App;
