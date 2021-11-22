825757bb002b2e6708f12d3a68cef1015c63e276
lazy-sky <1cielo@naver.com>
2021-11-22 14:59:35 +0900

    조건부 렌더링 - && 연산자

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -5,6 +5,7 @@ import Wrapper from './Wrapper';
 function App() {
   return (
     <Wrapper>
+      {/* isSpecial에서 값 설정을 생략해도 true로 설정한 것으로 간주한다. */}
       <Hello name="react" color="red" isSpecial={true}/>
       <Hello color="pink"/>
     </Wrapper>
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Hello({ color, name, isSpecial }) {
   return ( 
     <div style={{color}}>
-      { isSpecial ? <b>*</b> : null }
+      { isSpecial && <b>*</b> }
       Hello {name}
     </div>
   )
