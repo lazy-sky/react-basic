f6ba8503c3f9ff713dbf247b7fe287d71bf84258
lazy-sky <1cielo@naver.com>
2021-11-23 07:52:34 +0900

    배열에 항목 추가

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,16 +1,24 @@
-// useRef의 또 다른 역할
-// 컴포넌트 안에서 조회 및 수정할 수 있는 변수 관리
-// useRef로 관리되는 변수는 값이 바뀌어도 컴포넌트가 리렌더링되지 않는다.
-// 이를 활용하여 다음과 같은 값을 관리할 수 있다.
-// - setTimeout, setInterval을 통해서 만들어진 id
-// - 외부 라이브러리를 사용하여 생성된 인스턴스
-// - scroll 위치
-
-import { useRef } from 'react'
+import { useState, useRef } from 'react'
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  })
+
+  const { username, email } = inputs
+
+  const onChange = e => {
+    const { name, value } = e.target
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -26,7 +34,7 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ]
+  ])
 
   const nextId = useRef(4)
 
@@ -34,11 +42,32 @@ function App() {
     // 나중에 구현할 배열에 항목을 추가하는 로직
     // ...
 
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+
+    setUsers([...users, user])
+
+    setInputs({
+      username: '',
+      email: ''
+    })
+
     nextId.current += 1
   }
 
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
