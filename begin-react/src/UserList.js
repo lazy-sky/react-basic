7a4a187ca6cf2b6873b2202a3c285dfbe3d5be64
lazy-sky <1cielo@naver.com>
2021-11-23 07:20:11 +0900

    Rendering Array

---
+++ begin-react/src/UserList.js
@@ -0,0 +1,37 @@
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username} <span>{user.email}</span></b>
+    </div>
+  )
+}
+
+function UserList() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]
+
+  return (
+    <div>
+      {users.map((user, index) => (
+        <User user={user} key={index}/>
+      ))}
+    </div>
+  )
+}
+
+export default UserList
