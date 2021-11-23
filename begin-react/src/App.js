ee41ec67a5fdee9c0181a4b20c9385562d86104e
lazy-sky <1cielo@naver.com>
2021-11-23 17:35:19 +0900

    useReducer - 리팩토링, 복잡한 상태에 적합

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useMemo, useCallback } from 'react'
+import { useRef, useMemo, useCallback, useReducer } from 'react'
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,33 +7,13 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  })
+  },
 
-  const { username, email } = inputs
-
-  // 함수형 업데이트를 하게 되면, setUsers에 등록하는 콜백함수의 파라미터에서 최신 users를 참조할 수 있다.
-  // 그래서 deps에 users를 넣지 않아도 된다.
-  // 이로써 특정 항목을 수정하게 될 때, 해당 항목만 리렌더링된다.
-  // cf) 리액트 개발자 도구의 버그인지 createUser도 렌더링되는 것처럼 보이는데,
-  // 로그를 찍어보면 렌더링 되지 않고 있는 걸 확인할 수 있다.
-  
-  // 리액트 개발시 useCallback, useMemo, React.memo는 컴포넌트의 성능을 실제로 개선할 수 있는 상황에서만 써야 한다.
-  // - 렌더링 최적화하지 않을 컴포넌트에 React.memo를 사용하는 것은 불필요한 props 비교만 하게 한다.
-
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value
-      });
-    }, [])
-
-  const [users, setUsers] = useState([
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -52,39 +32,85 @@ function App() {
       email: 'liz@example.com',
       active: false
     }
-  ])
+  ]
+}
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      }
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(
+          user => user.id === action.id
+          ? { ...user, active: !user.active}
+          : user)
+      }
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      return state
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { users } = state
+  const { username, email } = state.inputs
   const nextId = useRef(4)
 
+  const onChange = useCallback(e => {
+    const { name, value } = e.target 
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, [])
+
   const onCreate = useCallback(() => {
-      const user = {
-      id: nextId.current,
-      username,
-      email
-    }
-
-    setUsers([...users, user])
-
-    setInputs({
-      username: '',
-      email: ''
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username, 
+        email
+      }
     })
 
     nextId.current += 1
-  }, [username, email]) 
-
-  const onRemove = useCallback(id => {
-    setUsers(users.filter(user => user.id !== id))
-  }, []) 
+  }, [username, email])
 
   const onToggle = useCallback(id => {
-    setUsers(users.map(
-      user => user.id === id ? { ...user, active: !user.active } : user
-    ))
-  }, []) 
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    })
+  }, [])
 
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    })
+  }, [])
 
-  const count = useMemo(()=> countActiveUsers(users), [users])
+  const count = useMemo(() => countActiveUsers(users), [users])
 
   return (
     <>
@@ -94,10 +120,15 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
       <div>활성 사용자 수: {count}</div>
     </>
   );
 }
 
 export default App;
+
+// useReducer vs useState
+// 정답은 없다.
+// 컴포넌트에서 관리하는 값이 딱 하나고, 그 값이 단순하다면 useState가 편할 것이다.
+// 반대로 관리하는 값이 여러 개고, 상태 구조가 복잡하다면 useReducer가 편할 수 있다.
