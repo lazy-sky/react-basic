1b44040571804d6d5eacfd2bb430afcfdfca120d
lazy-sky <1cielo@naver.com>
2021-11-23 11:43:37 +0900

    useEffect with empty deps

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react'
+
+// useEffect의 첫 번째 파라미터는 함수, 두 번째는 의존값이 들어있는 배열 deps를 넣는다.
+// 빈 배열을 넣으면 컴포넌트가 처음 나타날 때만 함수가 호출된다.
 function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남')
+
+    // useEffect는 함수를 반환활 수 있는데 이를 cleanup 함수라고 부른다.
+    // useEffect에 대한 뒷처리 느낌으로, 컴포넌트가 사라질 때 호출된다.
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐')
+    }
+  }, [])
+
   return (
     <div>
       <b style={{
