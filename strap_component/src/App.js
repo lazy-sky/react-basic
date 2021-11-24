981714f795b1746033c839a853cc0557e40600bd
lazy-sky <1cielo@naver.com>
2021-11-24 19:38:44 +0900

    Component test - Container, Input

--- strap_component/src/App.js
+++ strap_component/src/App.js
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Input } from 'reactstrap'
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Input></Input>
+      </Container>
     </div>
   );
 }
