a2cb0a673cc840e5d7cef3af2e15ff1103c87938
lazy-sky <1cielo@naver.com>
2021-11-24 18:52:18 +0900

    Error handling - defaultProps

--- begin-react/src/ErrorTest.js
+++ begin-react/src/ErrorTest.js
@@ -3,6 +3,7 @@ import React from 'react';
 function ErrorTest({ test }) {
   if (!test) {
     return null;
+    // retrun <div>로딩중</div>;
   }
 
   return (
@@ -18,3 +19,13 @@ function ErrorTest({ test }) {
 }
 
 export default ErrorTest;
+
+// 전달해야할 props를 전달하지 않아도 에러가 날 것이다. 
+// 이를 방지하기 위해 defaultProps를 설정해줄 수 있다.
+ErrorTest.defaultProps = {
+  onToggle: () => {
+    console.warn('onToggle is missing!');
+  }
+};
+
+// 다른 방법으로는 PropTypes를 사용하는 것이 있다. 다만 다소 귀찮기 때문에 타입스크립트와 같이 쓰는 것이 권장된다.
