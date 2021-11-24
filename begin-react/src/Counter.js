e11057a997125e3dbef8b2472c2b995c67e9be08
lazy-sky <1cielo@naver.com>
2021-11-24 17:48:30 +0900

    Class Component - 커스텀 메소드

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -36,9 +36,30 @@
 import { Component } from 'react';
 
 class Counter extends Component {
-  // 보통 클래스에서 커스텀 메서드는 handle... 로 작명한다.
+  
+  // 1. constructor에서 bind, 가장 일반적인 방법
+  constructor(props) {
+    super(props);
+    this.handleIncrease = this.handleIncrease.bind(this);
+    this.handleDecrease = this.handleDecrease.bind(this);
+  }
+
+  // 2. 화살표 함수
+  // handleIncrease = () => {
+  //   console.log('increase');
+  //   console.log(this);
+  // };
+
   handleIncrease() {
     console.log('increase');
+
+    console.log(this); // 그냥 쓰면 undefined
+    // 버튼에 이벤트로 등록하게 되는 과정에서 각 메서드와 컴포넌트 인스턴스의 관계가 끊기기 때문이다.
+    // 해결 방법은 3가지다.
+    // 1. constructor에서 bind, 가장 일반적인 방법
+    // 2. 화살표 함수, CRA 프로젝트에서는 이 방법을 가장 많이 사용한다. 편하다.
+    // 3. onClick에서 새로운 함수를 만들어 전달, 컴포넌트 최적화에 까다로워서 권장되지 않는다.
+    // e.g., <button onClick={() => this.handleIncrease()}>+1</button>
   }
 
   handleDecrease() {
@@ -49,8 +70,8 @@ class Counter extends Component {
     return (
       <div>
         <h1>0</h1>
-        <button>+1</button>
-        <button>-1</button>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
       </div>
     );
   }
