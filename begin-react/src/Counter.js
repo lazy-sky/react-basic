43240a1033caf64a0ce798bb043b496270937450
lazy-sky <1cielo@naver.com>
2021-11-24 17:39:00 +0900

    Class Component

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,49 +1,59 @@
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 
-// useReducer는 useState과는 또 다른 상태 관리 방법이다. 
-// 이 Hook을 이용하면 컴포넌트의 상태 업데이트 로직을 컴포넌트에서 분리시킬 수 있다.
-// 상태 업데이트 로직을 컴포넌트 바깥에 작성할 수도 있다, 심지어 다른 파일에서까지.
+// function reducer(state, action) {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     default:
+//       return state
+//   }
+// }
 
-// reducer는 현재 상태와 액션 객체를 파라미터로 받아와서 새로운 상태를 반환해주는 함수다.
-// reduecer에서 반환하는 상태는 곧 컴포넌트가 지닐 새로운 상태가 된다.
+// function Counter() {
+//   const [number, dispatch] = useReducer(reducer, 0);
 
-// 사용법은,
-// const [state, dispatch] = useReducer(reducer, initialState)
-// 여기서 state는 앞으로 컴포넌트에서 사용할 수 있는 상태,
-// dispatch는 액션을 발생시키는 함수라 할 수 있다. 
-// useReducer의 첫 번째 파라미터는 reducer함수, 두 번째는 초기 상태다.
-
-function reducer(state, action) {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
-
-function Counter() {
-  const [number, dispatch] = useReducer(reducer, 0);
-
-  const onIncrease = () => {
-    dispatch({type: 'INCREMENT'})
-  }
+//   const onIncrease = () => {
+//     dispatch({type: 'INCREMENT'})
+//   }
 
   
-  const onDecrease = () => {
-    dispatch({type: 'DECREMENT'})
+//   const onDecrease = () => {
+//     dispatch({type: 'DECREMENT'})
 
+//   }
+
+//   return (
+//     <div>
+//       <h1>{ number }</h1>
+//       <button onClick={onIncrease}>+1</button>
+//       <button onClick={onDecrease}>-1</button>
+//     </div>
+//   );
+// }
+
+import { Component } from 'react';
+
+class Counter extends Component {
+  // 보통 클래스에서 커스텀 메서드는 handle... 로 작명한다.
+  handleIncrease() {
+    console.log('increase');
   }
 
-  return (
-    <div>
-      <h1>{ number }</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
+  handleDecrease() {
+    console.log('decrease');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>0</h1>
+        <button>+1</button>
+        <button>-1</button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,12 +1,30 @@
-import React from 'react'
+// function Hello({ color, name, isSpecial }) {
+//   return ( 
+//     <div style={{color}}>
+//       { isSpecial && <b>*</b> }
+//       Hello {name}
+//     </div>
+//   )
+// }
 
-function Hello({ color, name, isSpecial }) {
-  return ( 
-    <div style={{color}}>
-      { isSpecial && <b>*</b> }
-      Hello {name}
-    </div>
-  )
+// 이제 잘 사용하지는 않지만 만나게 될 때를 대비하여 클래스 컴포넌트에 대해 정리하고자 한다.
+
+// 클래스 컴포넌트에서는 render() 메서드가 꼭 있어야 한다. 이 메서드에서 렌더링하고 싶은 JSX를 반환하면 된다.
+// props를 조회할 땐 this.props를 조회하면 된다.
+// defaultProps는 함수 컴포넌트와 같은 방식 혹은 클래스 내부 static 키워드로 선언할 수 있다.
+
+import { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
+      <div style={color}>
+        {isSpecial && <b>*</b>}
+        Hello {name}
+      </div>
+    );
+  }
 }
 
 Hello.defaultProps = {
