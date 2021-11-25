43fcb482323aac147d335fcd1e2456eff671a293
lazy-sky <1cielo@naver.com>
2021-11-25 22:28:56 +0900

    Add size classes

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -1,11 +1,13 @@
-import "./App.css";
-import Button from "./components/Button";
+import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button>BUTTON</Button>
+        <Button size="large">BUTTON-L</Button>
+        <Button>BUTTON-M</Button>
+        <Button size="small">BUTTON-S</Button>
       </div>
     </div>
   );
--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -1,7 +1,8 @@
-import "./Button.scss";
+import classNames from 'classnames';
+import './Button.scss';
 
 export default function Button({ children, size }) {
-  return <button className={`Button ${size}`}>{children}</button>;
+  return <button className={classNames('Button', size)}>{children}</button>;
 }
 
 // className에 CSS 클래스 이름을 동적으로 넣는 방법
@@ -9,3 +10,7 @@ export default function Button({ children, size }) {
 // 2. className={`Button ${size}`}
 // 3. 조건부로 CSS 클래스를 넣어주고 싶을 땐 위와 같이 문자열을 직접 조합하는 것보단,
 // classnames 라이브러리를 사용하는 것이 더 편하다.
+
+Button.defaultProps = {
+  size: 'medium',
+};
