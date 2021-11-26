38818815ca1febaa8a56c19fe5a30b21f9c9ca8d
lazy-sky <1cielo@naver.com>
2021-11-26 13:30:28 +0900

    outline buttons

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="buttons">
         <Button size="large">BUTTON</Button>
-        <Button>BUTTON</Button>
+        <Button out>BUTTON</Button>
         <Button size="small">BUTTON</Button>
       </div>
       <div className="buttons">
@@ -24,6 +24,17 @@ function App() {
         </Button>
         <Button color="pink">BUTTON</Button>
         <Button size="small" color="pink">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
           BUTTON
         </Button>
       </div>
--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -1,9 +1,11 @@
 import classNames from 'classnames';
 import './Button.scss';
 
-export default function Button({ children, size, color }) {
+export default function Button({ children, size, color, outline }) {
   return (
-    <button className={classNames('Button', size, color)}>{children}</button>
+    <button className={classNames('Button', size, color, { outline })}>
+      {children}
+    </button>
   );
 }
 
