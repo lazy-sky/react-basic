1e718a9373d32d8c0cf154f0cfa2b09745ba80bd
lazy-sky <1cielo@naver.com>
2021-11-26 16:55:09 +0900

    Make Checkbox

--- styling-with-css-module/src/App.js
+++ styling-with-css-module/src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
-import Box from "./Box";
+import { useState } from "react";
+
+import CheckBox from "./components/CheckBox";
 
 function App() {
+  const [check, setCheck] = useState(false);
+
+  const onChange = (e) => {
+    setCheck(e.target.checked);
+  };
+
   return (
     <div className="App">
-      {/* Box_Box__2aocn 라고 나옴 */}
-      <Box />
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b>
+        {check ? "true" : "false"}
+      </p>
     </div>
   );
 }
--- styling-with-css-module/src/Box.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Box.module.css";
-
-function Box() {
-  return <div className={styles.Box}>{styles.Box}</div>;
-}
-
-export default Box;
