e817c71e7f4fe2da4656c09e3b72264dde992c22
lazy-sky <1cielo@naver.com>
2021-11-26 21:44:48 +0900

    fullWidthStyle

--- styling-with-styled-components/src/components/Button.js
+++ styling-with-styled-components/src/components/Button.js
@@ -49,6 +49,19 @@ const sizeStyles = css`
   `}
 `;
 
+const fullWidthStyle = css`
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+      justify-content: center;
+      & + & {
+        margin-left: 0;
+        margin-top: 1rem;
+      }
+    `}
+`;
+
 const StyledButton = styled.button`
   /* 공통 스타일 */
   display: inline-flex;
@@ -71,11 +84,19 @@ const StyledButton = styled.button`
   & + & {
     margin-left: 1rem;
   }
+
+  ${fullWidthStyle}
 `;
 
-function Button({ children, color, size, ...rest }) {
+function Button({ children, color, size, outline, fullWidth, ...rest }) {
   return (
-    <StyledButton color={color} size={size} {...rest}>
+    <StyledButton
+      color={color}
+      size={size}
+      outline={outline}
+      fullWidth={fullWidth}
+      {...rest}
+    >
       {children}
     </StyledButton>
   );
