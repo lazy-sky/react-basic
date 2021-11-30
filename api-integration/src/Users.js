a428f6452926640efe6dbba7c5f4822758e89465
lazy-sky <1cielo@naver.com>
2021-11-30 17:05:45 +0900

    재사용을 위한 커스텀 훅으로 리팩토링

--- api-integration/src/Users.js
+++ api-integration/src/Users.js
@@ -1,52 +1,15 @@
-import { useReducer, useEffect } from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case 'ERROR':
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users',
+  );
+  return response.data;
 }
 
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users',
-      );
-      dispatch({ type: 'SUCCESS', data: response.data });
-    } catch (e) {
-      dispatch({ type: 'ERROR', error: e });
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const [state, refetch] = useAsync(getUsers, []);
 
   const { loading, data: users, error } = state;
 
@@ -56,7 +19,7 @@ function Users() {
 
   return (
     <>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
---
+++ api-integration/src/useAsync.js
@@ -0,0 +1,58 @@
+// 데이터를 요청해야 할 때마다 리듀서를 작성하는 것은 번거롭다.
+// 반복되는 코드를 작성하는 대신, 커스텀 Hook을 만들어 요청 상태 관리 로직을 쉽게 재사용할 수 있다.
+
+import { useReducer, useEffect } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+// 첫 번째 파라미터는 API 요청을 시작하는 함수,
+// 두 번째는 useEffect에서 deps로 활용될 배열, 현재는 불필요하므로(처음 렌더링할 때만 필요하므로) 기본값은 빈 배열.
+function useAsync(callback, deps = []) {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: false,
+  });
+
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' });
+    try {
+      const data = await callback();
+      dispatch({ type: 'SUCCESS', data });
+    } catch (error) {
+      dispatch({ type: 'ERROR', error });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+
+  return [state, fetchData];
+}
+
+export default useAsync;
