440d00f1a5b7ae38630b271299568f899c474433
lazy-sky <1cielo@naver.com>
2021-11-30 16:20:31 +0900

    refact: useState -> useReducer

--- api-integration/src/Users.js
+++ api-integration/src/Users.js
@@ -1,35 +1,54 @@
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
 import axios from 'axios';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  // 요청에 대한 상태를 관리할 땐 다음 3가지 상태를 관리해줘야 한다.
-  // 1. 요청의 결과
-  // 2. 로딩 상태
-  // 3. 에러
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
   const fetchUsers = async () => {
     try {
-      // 요청 시작시 error, users를 초기화하고, loading을 true로 바꾼다.
-      setError(null);
-      setUsers(null);
-      setLoading(true);
-
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users',
       );
-      setUsers(response.data);
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!!</div>;
