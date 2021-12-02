a4e17f6584a478c39d0ff8f5fbb0e3357f3264f5
lazy-sky <1cielo@naver.com>
2021-12-02 22:35:06 +0900

    Link

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
@@ -8,9 +8,21 @@ import About from "./About";
 // 일단은 Switch가 Routes로, component가 element로 바뀌었다.
 // exact props가 사라졌다.
 
+// Link는 다른 주소로 이동시키는 컴포넌트다.
+// a 태그가 페이지를 이동시키면서 페이지를 아예 새로 불러오는 것과 달리,
+// Link는 HTML5 History API를 사용하여 브라우저의 주소만 바꿀뿐 페이지를 새로 불러오지는 않는다.
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
