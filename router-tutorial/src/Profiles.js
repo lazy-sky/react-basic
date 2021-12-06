240a432bb90657110d017586d4fb599e90075121
lazy-sky <1cielo@naver.com>
2021-12-06 14:40:55 +0900

    NavLink

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const Profiles = () => {
   return (
@@ -6,10 +6,28 @@ const Profiles = () => {
       <h3>유저 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/sky">sky</Link>
+          {/* NavLink는 Link와 유사한데, 만약 현재 경로와 Link에서 사용하는 경로가 일치하는 경우,
+          특정 스타일 혹은 클래스를 적용할 수 있는 컴포넌트다. */}
+          <NavLink
+            to="/profiles/sky"
+            style={({ isActive }) => ({
+              color: isActive ? "white" : "blue",
+              background: isActive ? "black" : "white",
+            })}
+          >
+            sky
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/john">john</Link>
+          <NavLink
+            to="/profiles/john"
+            style={({ isActive }) => ({
+              color: isActive ? "white" : "blue",
+              background: isActive ? "black" : "white",
+            })}
+          >
+            john
+          </NavLink>
         </li>
       </ul>
 
