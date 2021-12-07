6e4dc9b96709385d82f4ab0ba04c4781f47d2fce
lazy-sky <1cielo@naver.com>
2021-12-07 13:32:27 +0900

    Init Provider

--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import rootReducer from './modules';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
