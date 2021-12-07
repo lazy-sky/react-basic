16574dc5e613d7fe828237036f2e8e3e89cc068c
lazy-sky <1cielo@naver.com>
2021-12-07 13:27:55 +0900

    Init store

--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
